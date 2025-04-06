bab5b16551eed2ccf7acfa18f5b4cd3419dc609b
Lucas Roseno <lucasroseno759@gmail.com>
2025-04-06 20:01:51 -0300

    configuração do vento concluída

--- src/config.cpp
+++ src/config.cpp
@@ -9,11 +9,11 @@ void config::executarSimulacao()
   imprimirMatriz();
 
   while (aindaTemFogo() && iteracao < 7)
-  
+
   {
     jaQueimou = vector<vector<bool>>(file.linhas, vector<bool>(file.colunas, false));
 
-    for (const auto& fogo : fogoInicial)
+    for (const auto &fogo : fogoInicial)
     {
       matriz[fogo.first][fogo.second] = 3;
     }
@@ -61,15 +61,123 @@ void config::propagacaoFogo()
 
 vector<vector<int>> config::espalharFogo(int posX, int posY, vector<vector<int>> novaMatriz)
 {
-  for (int i = 0; i < 4; i++)
+  switch (direcaoVento)
   {
-    if (posX + dx[i] >= 0 && posX + dx[i] < file.linhas &&
-        posY + dy[i] >= 0 && posY + dy[i] < file.colunas &&
-        novaMatriz[posX + dx[i]][posY + dy[i]] == 1)
+  case 0: // Sem vento
+    for (int i = 0; i < 4; i++)
     {
-      novaMatriz[posX + dx[i]][posY + dy[i]] = 2;
+      if (posX + dx[i] >= 0 && posX + dx[i] < file.linhas &&
+          posY + dy[i] >= 0 && posY + dy[i] < file.colunas &&
+          novaMatriz[posX + dx[i]][posY + dy[i]] == 1)
+      {
+        novaMatriz[posX + dx[i]][posY + dy[i]] = 2;
+      }
     }
-    
+    break;
+  case 1: // cima
+    if (posX - 1 >= 0 && novaMatriz[posX - 1][posY] == 1)
+    {
+      novaMatriz[posX - 1][posY] = 2;
+    }
+    break;
+  case 2: // baixo
+    if (posX + 1 < file.linhas && novaMatriz[posX + 1][posY] == 1)
+    {
+      novaMatriz[posX + 1][posY] = 2;
+    }
+    break;
+  case 3: // esquerda
+    if (posY - 1 >= 0 && novaMatriz[posX][posY - 1] == 1)
+    {
+      novaMatriz[posX][posY - 1] = 2;
+    }
+    break;
+  case 4: // direita
+    if (posY + 1 < file.colunas && novaMatriz[posX][posY + 1] == 1)
+    {
+      novaMatriz[posX][posY + 1] = 2;
+    }
+    break;
+  case 5: // cima e esquerda
+    if (posX - 1 >= 0 && posY - 1 >= 0 && novaMatriz[posX - 1][posY - 1] == 1)
+    {
+      novaMatriz[posX - 1][posY - 1] = 2;
+    }
+    break;
+  case 6: // cima e direita
+    if (posX - 1 >= 0 && posY + 1 < file.colunas && novaMatriz[posX - 1][posY + 1] == 1)
+    {
+      novaMatriz[posX - 1][posY + 1] = 2;
+    }
+    break;
+  case 7: // baixo e esquerda
+    if (posX + 1 < file.linhas && posY - 1 >= 0 && novaMatriz[posX + 1][posY - 1] == 1)
+    {
+      novaMatriz[posX + 1][posY - 1] = 2;
+    }
+    break;
+  case 8: // baixo e direita
+    if (posX + 1 < file.linhas && posY + 1 < file.colunas && novaMatriz[posX + 1][posY + 1] == 1)
+    {
+      novaMatriz[posX + 1][posY + 1] = 2;
+    }
+    break;
+  case 9: // cima, esquerda e direita
+    if (posX - 1 >= 0 && novaMatriz[posX - 1][posY] == 1)
+    {
+      novaMatriz[posX - 1][posY] = 2;
+    }
+    if (posY - 1 >= 0 && novaMatriz[posX][posY - 1] == 1)
+    {
+      novaMatriz[posX][posY - 1] = 2;
+    }
+    if (posY + 1 < file.colunas && novaMatriz[posX][posY + 1] == 1)
+    {
+      novaMatriz[posX][posY + 1] = 2;
+    }
+    break;
+  case 10: // cima, baixo e esquerda
+    if (posX - 1 >= 0 && novaMatriz[posX - 1][posY] == 1)
+    {
+      novaMatriz[posX - 1][posY] = 2;
+    }
+    if (posX + 1 < file.linhas && novaMatriz[posX + 1][posY] == 1)
+    {
+      novaMatriz[posX + 1][posY] = 2;
+    }
+    if (posY - 1 >= 0 && novaMatriz[posX][posY - 1] == 1)
+    {
+      novaMatriz[posX][posY - 1] = 2;
+    }
+    break;
+  case 11: // cima, baixo e direita
+    if (posX - 1 >= 0 && novaMatriz[posX - 1][posY] == 1)
+    {
+      novaMatriz[posX - 1][posY] = 2;
+    }
+    if (posX + 1 < file.linhas && novaMatriz[posX + 1][posY] == 1)
+    {
+      novaMatriz[posX + 1][posY] = 2;
+    }
+    if (posY + 1 < file.colunas && novaMatriz[posX][posY + 1] == 1)
+    {
+      novaMatriz[posX][posY + 1] = 2;
+    }
+    break;
+  case 12: // baixo, esquerda e direita
+    if (posX + 1 < file.linhas && novaMatriz[posX + 1][posY] == 1)
+    {
+      novaMatriz[posX + 1][posY] = 2;
+    }
+    if (posY - 1 >= 0 && novaMatriz[posX][posY - 1] == 1)
+    {
+      novaMatriz[posX][posY - 1] = 2;
+    }
+    if (posY + 1 < file.colunas && novaMatriz[posX][posY + 1] == 1)
+    {
+      novaMatriz[posX][posY + 1] = 2;
+    }
+    break;
   }
   return novaMatriz;
 }
--- src/config.hpp
+++ src/config.hpp
@@ -22,10 +22,16 @@ private:
     vector<pair<int, int>> fogoInicial;
     vector<pair<int, int>> apagarFogo;
 
+    // Direções: sem vento = 0, acima = 1, baixo = 2, esquerda = 3, direita = 4
+    // o resto está no .cpp
+    short int direcaoVento = 12;
+
+
 public:
     void executarSimulacao();
     void imprimirMatriz();
     void propagacaoFogo();
     vector<vector<int>> espalharFogo(int posX, int posY, vector<vector<int>> novaMatriz);
     bool aindaTemFogo();
+    short int definirDirecaoVento();
 };;
